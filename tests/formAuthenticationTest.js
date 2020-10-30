1bf2d9aa6b09faccee47db1c8f3376abe2d8c422
Juraj Cidorik <juraj.cidorik@qats.sk>
2020-10-30 10:59:50 +0100

    formAuthenticationTest

--- tests/formAuthenticationTest.js
+++ tests/formAuthenticationTest.js
@@ -1,9 +1,23 @@
 import {Selector} from 'testcafe';
 
-fixture("The Internet Herokuapp")
+const usernameInput = Selector('input#username');
+const passwordInput = Selector('input#password');
+const button = Selector('button');
+const logoutLink = Selector('a[href="/logout"]');
+const successMessage = Selector('div.success');
+
+fixture("Form authentication login and logout")
     .page("https://the-internet.herokuapp.com/")
 
-test.skip("Form authentication test", async t => {
+test.only("Form authentication login/logout test", async t => {
+    const user = "tomsmith";
+    const pwd = "SuperSecretPassword!";
     await t
         .navigateTo("/login")
-})
+        .typeText(usernameInput, user)
+        .typeText(passwordInput, pwd)
+        .click(button)
+        .expect(successMessage.innerText).contains("You logged into a secure area!")
+        .click(logoutLink)
+        .expect(successMessage.innerText).contains("You logged out of the secure area!")
+});
